5d7d6bb91625383d991a229ee4d9adf46dbe4abc
Vanderlei Zerbinati <114303351+VanzBeats@users.noreply.github.com>
2024-03-29 12:11:40 -0300

    Create index.php

---
+++ index.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>PHP</title>
+</head>
+<body>
+    <h1> Exercícios PHP</h1>
+    <h2>Vanderlei de Oliveira Zerbinati Martins</h2>
+    <ul>
+        <li><a href="PHP Repositório/exercicio1.php">Exercício 1</a></li>
+    </ul>
+</body>
+</html>
